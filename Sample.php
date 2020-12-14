ea24c96e7adb0c02cdff4253db8efaa623f65665
Hari Prakash <56472426+hari-praks@users.noreply.github.com>
2020-12-14 11:02:48 +0530

    Add files via upload

---
+++ Sample.php
@@ -0,0 +1,27 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Academic details</title>
+</head>
+<body bgcolor="#0055ff" text="white">
+	<?php
+
+echo "<br><h1>I am from second year</h1><br>";
+echo "<h5><blockspace>Completed C,c++,python</blockspace></h5><br><br>";
+echo "Studying web dev currently<br><br>";
+echo "<b><u><h2>Cgpa:9</b></u></h2>";
+echo "<marquee><h1>I am good boy</h1></marquee>";
+echo "<br><br><pre>this is pre tag
+it prints as it is 
+New to php so checking it</pre>";
+ print <<<END
+   This uses the "here document" syntax to output
+   multiple lines with $variable interpolation. Note
+   that the here document terminator must appear on a
+   line with just a semicolon no extra whitespace!
+   END;
+?>
+
+</body>
+</html>
